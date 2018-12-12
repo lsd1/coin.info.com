9e568d12ce1e9af4d4493751777a00bc1b68d2d5
stone <2416711130@qq.com>
2018-12-13 01:14:51 +0800

    优化

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var express = require('express'),
     app = express(),
-    server = require('http').createServer(app);
-    app.use('/', express.static(__dirname));
+    server = require('http').createServer(app),
+    path = require('path'),
+    history = require('connect-history-api-fallback');
+    app.use(history()).use(express.static(path.join(__dirname)));
     server.listen(process.env.PORT || 80);
